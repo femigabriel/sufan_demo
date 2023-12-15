788086dbac3a79fa34f31773e325eed0a172f4a4
Femi Gabriel <timfemi@gmail.com>
2023-12-15 12:15:10 +0100

    purchase form fixes

--- src/pages/User/Investments/Purchase/PurchaseForm.jsx
+++ src/pages/User/Investments/Purchase/PurchaseForm.jsx
@@ -1,10 +1,28 @@
+
 import React, { useState } from "react";
 import { Input } from "antd";
 import { Button } from "@mui/material";
 import theme from "../../../../theme";
 import TextWithLinesBg from "../../../../components/TextWithLinesBg";
+import { useNavigate } from "react-router-dom";
 
 export const PurchaseForm = () => {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [errorMessage, setErrorMessage] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!password || !email || !phone || !firstName || !lastname) {
+      setErrorMessage(true);
+      return;
+    }
+    navigate("/user/investments");
+  };
   return (
     <div>
       <main className="pt10 lg:pt-0 max-w-[1170px] -mx-4 sm:-mx-6 md:mx-auto flex flex-col items-center">
@@ -18,6 +36,7 @@ export const PurchaseForm = () => {
             action=""
             // onSubmit={methods.handleSubmit(onSubmit)}
             // className="max-w mx-auto w-full flex flex-col gap-6 md:gap-10 "
+            onSubmit={handleSubmit}
             className="max-w-[512px] mx-auto w-full flex flex-col justifycenter gap-6 md:gap-10 "
           >
             <div className="w-full flex flex-col ">
@@ -29,7 +48,15 @@ export const PurchaseForm = () => {
                 placeholder="eample@lorem.com"
                 className="form-input w-full lg:w-[512px] lg:h-[54px] text-base "
                 pattern="^\S+@\S+\.\S+$"
+                onChange={(e) => setEmail(e.target.value)}
               />
+              {errorMessage && email.length <= 0 ? (
+                <label className="text-[#F13737] text-[0.750em]">
+                  This field is required
+                </label>
+              ) : (
+                ""
+              )}
             </div>
 
             <div className="w-full flex flex-col">
@@ -41,7 +68,15 @@ export const PurchaseForm = () => {
                 type="password"
                 className="form-input w-full lg:w-[512px] lg:h-[54px] text-base "
                 // pattern="^\S+@\S+\.\S+$"
+                onChange={(e) => setPassword(e.target.value)}
               />
+              {errorMessage && password.length <= 0 ? (
+                <label className="text-[#F13737] text-[0.750em]">
+                  This field is required
+                </label>
+              ) : (
+                ""
+              )}
             </div>
 
             <div className="w-full flex flex-col">
@@ -53,7 +88,15 @@ export const PurchaseForm = () => {
                 type="text"
                 className="form-input w-full lg:w-[512px] lg:h-[54px] text-base "
                 pattern="^(?=.{1,40}$)[a-zA-Z]+(?:[-'\s][a-zA-Z]+)*$"
+                onChange={(e) => setFirstName(e.target.value)}
               />
+              {errorMessage && firstName.length <= 0 ? (
+                <label className="text-[#F13737] text-[0.750em]">
+                  This field is required
+                </label>
+              ) : (
+                ""
+              )}
             </div>
 
             <div className="w-full flex flex-col">
@@ -65,7 +108,15 @@ export const PurchaseForm = () => {
                 type="text"
                 className="form-input w-full lg:w-[512px] lg:h-[54px] text-base "
                 pattern="^(?=.{1,40}$)[a-zA-Z]+(?:[-'\s][a-zA-Z]+)*$"
+                onChange={(e) => setLastname(e.target.value)}
               />
+              {errorMessage && lastname.length <= 0 ? (
+                <label className="text-[#F13737] text-[0.750em]">
+                  This field is required
+                </label>
+              ) : (
+                ""
+              )}
             </div>
 
             <div className="w-full flex flex-col">
@@ -77,10 +128,43 @@ export const PurchaseForm = () => {
                 className="form-input w-full lg:w-[512px] lg:h-[54px] text-base "
                 placeholder="+1 234567890"
                 pattern="^\+[1-9]{1}[0-9]{3,14}$"
+                onChange={(e) => setPhone(e.target.value)}
               />
+              {errorMessage && phone.length <= 0 ? (
+                <label className="text-[#F13737] text-[0.750em]">
+                  This field is required
+                </label>
+              ) : (
+                ""
+              )}
             </div>
-
-            <div className="flex justify-between">
+            <div className="flex gap-10 justify-center">
+              {/* <button
+                className="bg-[#243bbd] py-3  rounded-md"
+                style={{
+                  width: "188px",
+                  mt: "auto",
+                  color: "#ffff",
+                  border: " 1px solid #15A4FB",
+                  fontSize: "16px",
+                }}
+                href={`/user/investments/landing-page`}
+              >
+                Book a Session
+              </button> */}
+              <button
+                className="py-3 px-10 rounded-md"
+                style={{
+                  background: theme.palette.primary.mainGradient,
+                  width: "148px",
+                  mt: "auto",
+                  fontSize: "16px",
+                }}
+              >
+                Purchase
+              </button>
+            </div>
+            {/* <div className="flex justify-between">
               <span></span>
               <div className="flex my-7 justify-center gap-5">
                 <Button
@@ -99,7 +183,7 @@ export const PurchaseForm = () => {
                   Book a Session
                 </Button>
                 <Button
-                  href={`/user/investments/landing-page`}
+                  // href={`/user/investments`}
                   variant="contained"
                   style={{
                     background: theme.palette.primary.mainGradient,
@@ -107,15 +191,15 @@ export const PurchaseForm = () => {
                     mt: "auto",
                     fontSize: "16px",
                   }}
+                  onSubmit={handleSubmit}
                 >
                   Purchase
                 </Button>
               </div>
-            </div>
+            </div> */}
           </form>
         </div>
       </main>
     </div>
   );
 };
-
